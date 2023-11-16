57c8a2a450587af6558f6456db8c169a70bbccb8
Kvanatsu <20062000@inbox.ru>
2023-11-16 11:28:34 +0700

    END BITCH

--- designpro/catalog/urls.py
+++ designpro/catalog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import ApplicationListView, delete_application, ApplicationAllListView
-
 urlpatterns = [
     path('', ApplicationAllListView.as_view(), name='index'),
     path('register/', views.RegisterView.as_view(), name='register'),
